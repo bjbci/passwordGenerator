cbbdd512c17aeecd6cee8e1f6f1eb2aef8460d2b
Brian Bulowicz <bajebi.bb@gmail.com>
2022-09-17 17:34:56 -0500

    function finished

--- script3.js
+++ script3.js
@@ -1,15 +1,19 @@
 // Assignment Code
-const x =[]
-const n =[1,2,3,4,5,6,7,8,9,0]
-const uL =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-const lL =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const char =["!","@","#","$","%","^","&","*","(",")","-","=","+",]  
-const y = true
-const f = false
+ var expanding=[]
+ var s =[]
+ var num =[1,2,3,4,5,6,7,8,9,0]
+ var t=[]
+ var upLetter =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+ var l=[]
+ var lowLetter =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ var m=[]
+ var char =["!","@","#","$","%","^","&","*","(",")","-","=","+",]  
+let y = true
+let n = false
 /*var generateBtn =*/ document.querySelector("#generate").addEventListener('click',promptThis);
 
 function promptThis(){
-let numCharacters = prompt("How long do you want your password to be. 8-128 characters");
+let numCharacters = prompt("How long do you want your password to be", "8-128 characters");
 
   if ((numCharacters >=8 ) && (numCharacters <= 128)){
     console.log(numCharacters)
@@ -18,46 +22,71 @@ let numCharacters = prompt("How long do you want your password to be. 8-128 char
   prompt("you did not enter a valid number")
 }
 
-  let zeroNine = prompt("would you like to include numbers? "+"Y or N")
+  let zeroNine = prompt("would you like to include numbers? ","y or n")
   console.log(zeroNine)
 
-  if (y) {
-    expanding =x.concat(n)
+  if (zeroNine == "y") {
+    console.log(s)                            // s is empty
     console.log(expanding)
-  }else{
-    expanding +0
-  }
+     expanding =s.concat(num)                 // add array s to array num
+    console.log(expanding);
+  }else {
+    console.log("NO NUMBERS")
+    console.log(s);
+    expanding=s.concat(s)
+    console.log(expanding);
     
-  
-
-  let upperCase = prompt("would you like to include uppercase letters "+"Y or N")
- console.log(upperCase)
- if (y){
-  expanding =x.concat(uL)
+  }
 console.log(expanding)
+
+
+   let upperCase = prompt("would you like to include uppercase letters "+"Y or N")
+  console.log(upperCase)
+  if (upperCase=="y"){
+  console.log(t)                             // t is empty
+    console.log(expanding)
+   expanding =expanding.concat(upLetter)                   //add array t to uL
+ console.log(expanding)
+  }else{
+   console.log("NO UPPERCASE LETTERS")
+   console.log(upLetter);
+   expanding=expanding.concat(t)
+   console.log(expanding);
+   
+ }
+ console.log(expanding);
+
+
+  let lowerCase=prompt("would you like to include " +"lowercase letters "+"Y or N")
+  console.log(lowerCase)
+  if (lowerCase=="y"){
+  console.log(l)                          //l is empty
+  console.log(expanding)
+  expanding =expanding.concat(lowLetter)                  // add array l to lL
+ console.log(expanding)
+  }else{
+   console.log("NO LOWERCASE NUMBERS")
+   console.log(lowLetter);
+   expanding=expanding.concat(l)
+   console.log(expanding);
+ }
+
+  let characters=prompt("would you like to include special characters !@#$%^&*()" +"Y or N")
+ console.log(char)
+ if (characters=="y"){
+  console.log(m)                            // m is empty
+    console.log(expanding)
+   expanding =expanding.concat(char)                //add m to char
+ console.log(expanding)
  }else{
-  expanding +0
-}
+   console.log("NO CHARACTERS")
+   console.log(char);
+   expanding=expanding.concat(m)
+   console.log(expanding);
 
- let lowerCase=prompt("would you like to include " +"lowercase letters "+"Y or N")
- console.log(lowerCase)
- if (y){
-  expanding =x.concat(lL)
-console.log(expanding)
- }else{
-  expanding +0
-}
-
- let characters=prompt("would you like to include special characters !@#$%^&*()" +"Y or N")
-console.log(characters)
-if (y){
-  expanding =x.concat(char)
-console.log(expanding)
-}else{
-  expanding +0
-}
-}
-
+ }
+ }
+promptThis(console.log("Final expanding array" +expanding))
 // console.log(x)
 
 // expanding =x.concat(n)
@@ -116,7 +145,7 @@ var numCharacters;
 
 
 
-
+66
 
 
 
