ea79533f7ac4c93bcec8a47e126cd75d79e4e2a8
Brian Bulowicz <bajebi.bb@gmail.com>
2022-09-17 03:32:37 -0500

    second commit

--- script3.js
+++ script3.js
@@ -1,32 +1,84 @@
 // Assignment Code
-const all=[]
+const x =[]
 const n =[1,2,3,4,5,6,7,8,9,0]
-const uL =[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-const lL =[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+const uL =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const lL =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const char =["!","@","#","$","%","^","&","*","(",")","-","=","+",]  
-
+const y = true
+const f = false
 /*var generateBtn =*/ document.querySelector("#generate").addEventListener('click',promptThis);
 
 function promptThis(){
 let numCharacters = prompt("How long do you want your password to be. 8-128 characters");
 
-  if (numCharacters >= 8){
+  if ((numCharacters >=8 ) && (numCharacters <= 128)){
     console.log(numCharacters)
-  }else
+     }else{
   console.log("you did not enter a valid number")
+  prompt("you did not enter a valid number")
+}
 
   let zeroNine = prompt("would you like to include numbers? "+"Y or N")
   console.log(zeroNine)
 
+  if (y) {
+    expanding =x.concat(n)
+    console.log(expanding)
+  }else{
+    expanding +0
+  }
+    
+  
+
   let upperCase = prompt("would you like to include uppercase letters "+"Y or N")
  console.log(upperCase)
+ if (y){
+  expanding =x.concat(uL)
+console.log(expanding)
+ }else{
+  expanding +0
+}
 
  let lowerCase=prompt("would you like to include " +"lowercase letters "+"Y or N")
  console.log(lowerCase)
+ if (y){
+  expanding =x.concat(lL)
+console.log(expanding)
+ }else{
+  expanding +0
+}
 
  let characters=prompt("would you like to include special characters !@#$%^&*()" +"Y or N")
 console.log(characters)
+if (y){
+  expanding =x.concat(char)
+console.log(expanding)
+}else{
+  expanding +0
 }
+}
+
+// console.log(x)
+
+// expanding =x.concat(n)
+// console.log(expanding)
+
+// expanding =x.concat(uL)
+// console.log(expanding)
+
+// expanding =x.concat(lL)
+// console.log(expanding)
+
+// expanding =x.concat(char)
+// console.log(expanding)
+
+// expanding =x.concat(n).concat(uL).concat(lL)
+// console.log(expanding)
+
+// expanding =x.concat(n).concat(uL).concat(lL).concat(char)
+// console.log(expanding)
+
+// console.log(expanding)
 
 var numCharacters;
 //const uppercase=ASDFGHJKLPOIUYTREWQZXCVBNM;
